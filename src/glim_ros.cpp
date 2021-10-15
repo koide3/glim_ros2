b71a9b8b6e0b0b6f6403a6175d1cadbf3383c723
k.koide <k.koide@aist.go.jp>
2021-10-15 19:34:55 +0900

    remove adhoc viewer

--- src/glim_ros.cpp
+++ src/glim_ros.cpp
@@ -12,45 +12,29 @@
 #include <glim/util/config.hpp>
 #include <glim/preprocess/cloud_preprocessor.hpp>
 #include <glim/frontend/odometry_estimation.hpp>
+#include <glim/viewer/interactive_viewer.hpp>
 
 #include <glim_ros/raw_points.hpp>
-
-#include <glk/texture.hpp>
-#include <glk/texture_opencv.hpp>
-#include <glk/pointcloud_buffer.hpp>
-#include <glk/primitives/primitives.hpp>
-#include <guik/viewer/anonymous.hpp>
-#include <guik/viewer/light_viewer.hpp>
 
 class GlimROSNode : public rclcpp::Node {
 public:
   GlimROSNode() : Node("glim_ros") {
-    auto viewer = guik::LightViewer::instance();
-
     auto global_config = glim::GlobalConfig::instance();
     global_config->override_param<std::string>("global", "config_path", "/home/koide/ros2_ws/src/glim/config");
 
     preprocessor.reset(new glim::CloudPreprocessor);
     odometry_estimation.reset(new glim::OdometryEstimation);
+    interactive_viewer.reset(new glim::InteractiveViewer);
 
     using std::placeholders::_1;
     imu_sub = this->create_subscription<sensor_msgs::msg::Imu>("/livox/imu", 100, std::bind(&GlimROSNode::imu_callback, this, _1));
     image_sub = this->create_subscription<sensor_msgs::msg::Image>("/image", 10, std::bind(&GlimROSNode::image_callback, this, _1));
     points_sub = this->create_subscription<sensor_msgs::msg::PointCloud2>("/livox/lidar", 10, std::bind(&GlimROSNode::points_callback, this, _1));
-
-    timer = this->create_wall_timer(std::chrono::milliseconds(10), [this] { timer_callback(); });
   }
 
-  ~GlimROSNode() {
-  }
+  ~GlimROSNode() {}
 
 private:
-  void timer_callback() {
-    if (!guik::LightViewer::instance()->spin_once()) {
-      rclcpp::shutdown();
-    }
-  }
-
   void imu_callback(const sensor_msgs::msg::Imu::ConstSharedPtr msg) {
     const double imu_stamp = msg->header.stamp.sec + msg->header.stamp.nanosec / 1e9;
     const Eigen::Vector3d linear_acc(msg->linear_acceleration.x, msg->linear_acceleration.y, msg->linear_acceleration.z);
@@ -58,12 +42,7 @@ private:
     odometry_estimation->insert_imu(imu_stamp, linear_acc * 9.80665, angular_vel);
   }
 
-  void image_callback(const sensor_msgs::msg::Image::ConstSharedPtr msg) {
-    auto cv_image = cv_bridge::toCvCopy(msg, "bgr8");
-
-    auto viewer = guik::LightViewer::instance();
-    viewer->update_image("image", glk::create_texture(cv_image->image));
-  }
+  void image_callback(const sensor_msgs::msg::Image::ConstSharedPtr msg) { auto cv_image = cv_bridge::toCvCopy(msg, "bgr8"); }
 
   void points_callback(const sensor_msgs::msg::PointCloud2::ConstSharedPtr msg) {
     auto frame = glim_ros::RawPoints::extract(msg);
@@ -81,22 +60,8 @@ private:
     }
 
     auto preprocessed = preprocessor->preprocess(frame->stamp, frame->times, frame->points);
-    std::vector<glim::EstimationFrame::Ptr> marginalized_frames;
+    std::vector<glim::EstimationFrame::ConstPtr> marginalized_frames;
     auto estimated = odometry_estimation->insert_frame(preprocessed, marginalized_frames);
-
-    if(estimated == nullptr) {
-      return;
-    }
-
-    auto viewer = guik::LightViewer::instance();
-    for (const auto& frame : marginalized_frames) {
-      auto cloud_buffer = std::make_shared<glk::PointCloudBuffer>(frame->frame->points, frame->frame->size());
-      viewer->update_drawable(guik::anon(), cloud_buffer, guik::Rainbow(frame->T_world_lidar.cast<float>()));
-    }
-
-    auto cloud_buffer = std::make_shared<glk::PointCloudBuffer>(estimated->frame->points, estimated->frame->size());
-    viewer->update_drawable("current_coord", glk::Primitives::coordinate_system(), guik::VertexColor(estimated->T_world_lidar.cast<float>()));
-    viewer->update_drawable("current_points", cloud_buffer, guik::FlatColor(1.0, 0.5, 0.0, 1.0, estimated->T_world_lidar.cast<float>()).add("point_scale", 2.0f));
   }
 
 private:
@@ -107,6 +72,7 @@ private:
 
   std::unique_ptr<glim::CloudPreprocessor> preprocessor;
   std::unique_ptr<glim::OdometryEstimation> odometry_estimation;
+  std::unique_ptr<glim::InteractiveViewer> interactive_viewer;
 };
 
 int main(int argc, char** argv) {
