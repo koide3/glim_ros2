d8a16daaf981be7f371a5a2c440e42c2baca3d94
k.koide <k.koide@aist.go.jp>
2021-10-20 12:03:32 +0900

    async processing

--- src/glim_ros.cpp
+++ src/glim_ros.cpp
@@ -1,4 +1,5 @@
 #include <any>
+#include <deque>
 #include <thread>
 #include <iostream>
 #include <boost/format.hpp>
@@ -13,7 +14,12 @@
 
 #include <glim/util/config.hpp>
 #include <glim/preprocess/cloud_preprocessor.hpp>
+#include <glim/frontend/async_odometry_estimation.hpp>
+#include <glim/backend/async_sub_mapping.hpp>
+#include <glim/backend/async_global_mapping.hpp>
 #include <glim/frontend/odometry_estimation.hpp>
+#include <glim/backend/sub_mapping.hpp>
+#include <glim/backend/global_mapping.hpp>
 #include <glim/viewer/interactive_viewer.hpp>
 
 #include <glim_ros/raw_points.hpp>
@@ -24,25 +30,45 @@ public:
     auto global_config = glim::GlobalConfig::instance();
     global_config->override_param<std::string>("global", "config_path", "/home/koide/ros2_ws/src/glim/config");
 
+    latest_imu_stamp = 0.0;
+
     preprocessor.reset(new glim::CloudPreprocessor);
-    odometry_estimation.reset(new glim::OdometryEstimation);
+
+    odometry_estimation.reset(new glim::AsyncOdometryEstimation(std::shared_ptr<glim::OdometryEstimation>(new glim::OdometryEstimation)));
+    sub_mapping.reset(new glim::AsyncSubMapping(std::shared_ptr<glim::SubMapping>(new glim::SubMapping)));
+    global_mapping.reset(new glim::AsyncGlobalMapping(std::shared_ptr<glim::GlobalMapping>(new glim::GlobalMapping)));
+
     interactive_viewer.reset(new glim::InteractiveViewer);
 
+    // const std::string points_topic = "/camera/depth/color/points";
+    // const std::string imu_topic = "/camera/imu";
+    const std::string imu_topic = "/livox/imu";
+    const std::string points_topic = "/livox/lidar";
+    const std::string image_topic = "/image";
+
     using std::placeholders::_1;
-    imu_sub = this->create_subscription<sensor_msgs::msg::Imu>("/livox/imu", 100, std::bind(&GlimROSNode::imu_callback, this, _1));
-    image_sub = this->create_subscription<sensor_msgs::msg::Image>("/image", 10, std::bind(&GlimROSNode::image_callback, this, _1));
-    // points_sub = this->create_subscription<sensor_msgs::msg::PointCloud2>("/livox/lidar", 10, std::bind(&GlimROSNode::points_callback, this, _1));
-    points_sub = this->create_subscription<livox_interfaces::msg::CustomMsg>("/livox/lidar", 10, std::bind(&GlimROSNode::livox_points_callback, this, _1));
+    imu_sub = this->create_subscription<sensor_msgs::msg::Imu>(imu_topic, 100, std::bind(&GlimROSNode::imu_callback, this, _1));
+    image_sub = this->create_subscription<sensor_msgs::msg::Image>(image_topic, 10, std::bind(&GlimROSNode::image_callback, this, _1));
+    // points_sub = this->create_subscription<sensor_msgs::msg::PointCloud2>(points_topic, 10, std::bind(&GlimROSNode::points_callback, this, _1));
+    points_sub = this->create_subscription<livox_interfaces::msg::CustomMsg>(points_topic, 10, std::bind(&GlimROSNode::livox_points_callback, this, _1));
+
+    timer = this->create_wall_timer(std::chrono::milliseconds(1), std::bind(&GlimROSNode::timer_callback, this));
   }
 
   ~GlimROSNode() {}
 
 private:
   void imu_callback(const sensor_msgs::msg::Imu::ConstSharedPtr msg) {
+    const double imu_acc_scale_factor = 9.80665;
+
     const double imu_stamp = msg->header.stamp.sec + msg->header.stamp.nanosec / 1e9;
-    const Eigen::Vector3d linear_acc(msg->linear_acceleration.x, msg->linear_acceleration.y, msg->linear_acceleration.z);
+    const Eigen::Vector3d linear_acc = imu_acc_scale_factor * Eigen::Vector3d(msg->linear_acceleration.x, msg->linear_acceleration.y, msg->linear_acceleration.z);
     const Eigen::Vector3d angular_vel(msg->angular_velocity.x, msg->angular_velocity.y, msg->angular_velocity.z);
-    odometry_estimation->insert_imu(imu_stamp, linear_acc * 9.80665, angular_vel);
+    odometry_estimation->insert_imu(imu_stamp, linear_acc, angular_vel);
+    sub_mapping->insert_imu(imu_stamp, linear_acc, angular_vel);
+    global_mapping->insert_imu(imu_stamp, linear_acc, angular_vel);
+
+    latest_imu_stamp = imu_stamp;
   }
 
   void image_callback(const sensor_msgs::msg::Image::ConstSharedPtr msg) { auto cv_image = cv_bridge::toCvCopy(msg, "bgr8"); }
@@ -62,9 +88,14 @@ private:
       }
     }
 
-    auto preprocessed = preprocessor->preprocess(frame->stamp, frame->times, frame->points);
-    std::vector<glim::EstimationFrame::ConstPtr> marginalized_frames;
-    auto estimated = odometry_estimation->insert_frame(preprocessed, marginalized_frames);
+    frame_queue.push_back(frame);
+
+    while (!frame_queue.empty() && frame_queue.front()->stamp + 0.1 < latest_imu_stamp) {
+      const auto front = frame_queue.front();
+      auto preprocessed = preprocessor->preprocess(front->stamp, front->times, front->points);
+      odometry_estimation->insert_frame(preprocessed);
+      frame_queue.pop_front();
+    }
   }
 
   void livox_points_callback(const livox_interfaces::msg::CustomMsg::ConstSharedPtr msg) {
@@ -78,9 +109,35 @@ private:
       points[i] << point.x, point.y, point.z, 1.0;
     }
 
-    auto preprocessed = preprocessor->preprocess(stamp, times, points);
+    glim_ros::RawPoints::Ptr frame(new glim_ros::RawPoints);
+    frame->stamp = stamp;
+    frame->times = times;
+    frame->points = points;
+
+    frame_queue.push_back(frame);
+
+    while (!frame_queue.empty() && frame_queue.front()->stamp + 0.1 < latest_imu_stamp) {
+      const auto front = frame_queue.front();
+      auto preprocessed = preprocessor->preprocess(front->stamp, front->times, front->points);
+      odometry_estimation->insert_frame(preprocessed);
+      frame_queue.pop_front();
+    }
+  }
+
+  void timer_callback() {
+    std::vector<glim::EstimationFrame::ConstPtr> estimation_frames;
     std::vector<glim::EstimationFrame::ConstPtr> marginalized_frames;
-    auto estimated = odometry_estimation->insert_frame(preprocessed, marginalized_frames);
+    odometry_estimation->get_results(estimation_frames, marginalized_frames);
+
+    for(const auto& frame : marginalized_frames) {
+      sub_mapping->insert_frame(frame);
+    }
+
+    auto submaps = sub_mapping->get_results();
+    
+    for(const auto& submap: submaps) {
+      global_mapping->insert_submap(submap);
+    }
   }
 
 private:
@@ -89,8 +146,15 @@ private:
   std::any image_sub;
   std::any points_sub;
 
+  double latest_imu_stamp;
+  std::deque<glim_ros::RawPoints::ConstPtr> frame_queue;
+
   std::unique_ptr<glim::CloudPreprocessor> preprocessor;
-  std::unique_ptr<glim::OdometryEstimation> odometry_estimation;
+  std::unique_ptr<glim::AsyncOdometryEstimation> odometry_estimation;
+  std::unique_ptr<glim::AsyncSubMapping> sub_mapping;
+  std::unique_ptr<glim::AsyncGlobalMapping> global_mapping;
+
+
   std::unique_ptr<glim::InteractiveViewer> interactive_viewer;
 };
 
