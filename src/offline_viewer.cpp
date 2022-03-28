bd0d77e05d61405895a6de39ed7c9c50f52acc0e
k.koide <k.koide@aist.go.jp>
2022-03-28 11:37:47 +0900

    package path for offline viewer

--- src/offline_viewer.cpp
+++ src/offline_viewer.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <ament_index_cpp/get_package_share_directory.hpp>
+
 #include <glim/util/config.hpp>
 #include <glim/viewer/offline_viewer.hpp>
 
 int main(int argc, char** argv) {
-  const std::string config_path = "/home/koide/ros2_ws/src/glim/config";
+  const std::string config_ros_path = ament_index_cpp::get_package_share_directory("glim_ros") + "/config/glim_ros.json";
+  std::cout << "config_ros_path:" << config_ros_path << std::endl;
+  glim::Config config_ros(config_ros_path);
+
+  const std::string config_path = ament_index_cpp::get_package_share_directory("glim") + config_ros.param<std::string>("glim_ros", "config_path", "/config");
+  std::cout << "config_path:" << config_path << std::endl;
   glim::GlobalConfig::instance(config_path);
 
   glim::OfflineViewer viewer;
